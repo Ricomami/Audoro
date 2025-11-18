1bd04861679f8884bbe6a0f1222eae25f8c450d8
Ricomami <don.abraham01@gmail.com>
2025-11-18 13:36:11 -0600

    Se descomentaron las lineas en los modelos sequelize restantes.

--- backend/app/models/Funcion.js
+++ backend/app/models/Funcion.js
@@ -1,31 +1,31 @@
-// const { DataTypes } = require('sequelize');
-// const sequelize = require('../../db/mysql');
-// const Evento = require('./Evento');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../../db/mysql');
+const Evento = require('./Evento');
 
-// const Funcion = sequelize.define('Funcion', {
-//     id_funcion: { 
-//         type: DataTypes.INTEGER, 
-//         autoIncrement: true, 
-//         primaryKey: true 
-//     },
-//     evento_id: { type: DataTypes.INTEGER, allowNull: false },
-//     fecha_hora_funcion: { type: DataTypes.DATE, allowNull: false },
-//     estado: { 
-//         type: DataTypes.ENUM('Activo','Inactivo','Pendiente','Suspendido'), 
-//         defaultValue: 'Activo' 
-//     },
-// }, {
-//     tableName: 'funciones',
-//     timestamps: true,
-//     createdAt: 'created_at',
-//     updatedAt: 'updated_at'
-// });
+const Funcion = sequelize.define('Funcion', {
+    id_funcion: { 
+        type: DataTypes.INTEGER, 
+        autoIncrement: true, 
+        primaryKey: true 
+    },
+    evento_id: { type: DataTypes.INTEGER, allowNull: false },
+    fecha_hora_funcion: { type: DataTypes.DATE, allowNull: false },
+    estado: { 
+        type: DataTypes.ENUM('Activo','Inactivo','Pendiente','Suspendido'), 
+        defaultValue: 'Activo' 
+    },
+}, {
+    tableName: 'funciones',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+});
 
-// // Relación FK con Evento
-// Funcion.belongsTo(Evento, { foreignKey: 'evento_id' });
-// Evento.hasMany(Funcion, { foreignKey: 'evento_id' });
+// Relación FK con Evento
+Funcion.belongsTo(Evento, { foreignKey: 'evento_id' });
+Evento.hasMany(Funcion, { foreignKey: 'evento_id' });
 
-// module.exports = Funcion;
+module.exports = Funcion;
 
 //MODELO CON MONGOOSE
 
